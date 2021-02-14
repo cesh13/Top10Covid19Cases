505a97785361e30d98df9f421c1048fb2758ce47
Cesar Molina <cesh_m@hotmail.com>
2021-02-14 07:34:03 -0600

    Initial Commit

---
+++ Top10Covid19Cases/Top10Covid19Cases/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Top10Covid19Cases.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
